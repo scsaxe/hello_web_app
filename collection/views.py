078427314433d6f2579743309f9a0685b6bd8a0f
Sean Saxe <ssaxe@austin.utexas.edu>
2015-09-11 16:24:50 -0500

    Added more templates. Through chapter 5 of hello web app

--- collection/views.py
+++ collection/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    number = 6
+    return render(request, 'index.html',{
+        'number': number,
+    })
