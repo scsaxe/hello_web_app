0e427cb8c1f8e8fe29f4c1f2f3fca346bdfaaf84
Sean Saxe <ssaxe@austin.utexas.edu>
2015-09-11 16:55:33 -0500

    Added restaurant model and admin. Through chapter 6

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from collection.models import Restaurant
+
+
+class RestaurantAdmin(admin.ModelAdmin):
+    model = Restaurant
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Restaurant, RestaurantAdmin)
--- collection/models.py
+++ collection/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
