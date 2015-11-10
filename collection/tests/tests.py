2b8447d7cb4baf4874bb071a557d76bc61c8e350
Sean Saxe <ssaxe@austin.utexas.edu>
2015-11-09 20:31:22 -0600

    added tests. through chapter 12

--- collection/tests/tests.py
+++ collection/tests/tests.py
@@ -1,2 +1,11 @@
-from unittest import TestCase
+from django.test import TestCase
 
+
+class CollectionTest(TestCase):
+    def test_index(self):
+        r = self.client.get('/')
+        self.assertEqual(r.status_code, 200)
+
+    def test_no_logic_page(self):
+        r = self.client.get('/about/')
+        self.assertEqual(r.status_code, 200)
